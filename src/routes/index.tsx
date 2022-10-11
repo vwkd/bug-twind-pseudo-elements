9494747ecc377b99ca50aec9569e52a6e362b0a2
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-11 19:45:23 +0400

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="/reset.css" />
+      </Head>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
